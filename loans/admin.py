ee988d0c56710043ecb44aa428ab2392a03faf7d
Rishabh100ni <rishabhsoni10011@gmail.com>
2026-06-20 14:44:35 +0530

    Register LoanApplication model in Django Admin panel

--- loans/admin.py
+++ loans/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import LoanApplication
 
-# Register your models here.
+@admin.register(LoanApplication)
+class LoanApplicationAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'loan_type', 'loan_amount_required', 'status', 'created_at')
+    list_filter = ('status', 'loan_type')
+    search_fields = ('full_name', 'email', 'pan_number', 'mobile_number')
